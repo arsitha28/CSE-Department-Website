ad9386b374c0e5f2fda3b22a6e37bcc37a534c3f
arsitha20 <sarcse28@gmail.com>
2021-04-20 08:58:21 +0530

    Las commit

--- src/components/Labs/Labs.jsx
+++ src/components/Labs/Labs.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './labs.css';
 import Slide from 'react-reveal/Slide';
 import Zoom from 'react-reveal/Zoom';
 import Flip from 'react-reveal/Flip';
 import Fade from 'react-reveal/Fade';
+import './labs.css';
 
 
 
@@ -14,143 +14,140 @@ export default class Labs extends React.Component {
             <div>
 
 
-        <br/>
-        <br/>
-        <br/>
+            <br/>
+            <br/>
+            <br/>
         
 
-<div class="container" data-aos="fade-up">
+            <div class="container" data-aos="fade-up">
 
-<div class="row">
-  <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-<Flip left> <img src="assets/images/testimonials-4.jpg"></img> </Flip> 
-  </div>
-  <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
-   <Fade left>  <h3> Web Engineering Lab </h3> </Fade>
-   <Fade left> <p class="font-italic">
-       Description of the lab ,what students do in the lab
-      
-    </p> </Fade>
-   <Fade left> <ul>
-      <li><i class="ri-check-double-line"></i> program-1</li>
-      <li><i class="ri-check-double-line"></i> program-2</li>
-      <li><i class="ri-check-double-line"></i> program-3</li>
-      <li><i class="ri-check-double-line"></i> program-4</li>
-      <li><i class="ri-check-double-line"></i> program-5</li>
-      <li><i class="ri-check-double-line"></i> program-6</li>
+            <div class="row">
+              <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+                <Flip left> <img src="assets/images/model-img1.jpg"></img> </Flip> 
+              </div>
+              <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+                <Fade left>  <h3> Web Engineering Lab </h3> </Fade>
+                <Fade left> <p class="font-italic">
+                    Description of the lab ,what students do in the lab
+                    
+                  </p> </Fade>
+                <Fade left> <ul>
+                    <li><i class="ri-check-double-line"></i> program-1</li>
+                    <li><i class="ri-check-double-line"></i> program-2</li>
+                    <li><i class="ri-check-double-line"></i> program-3</li>
+                    <li><i class="ri-check-double-line"></i> program-4</li>
+                    <li><i class="ri-check-double-line"></i> program-5</li>
+                    <li><i class="ri-check-double-line"></i> program-6</li>
 
-    </ul> </Fade>
-    <Fade left> <p>
-      Lab capacity , Specifications of the lab
-      
-    </p> </Fade>
-  </div>
-</div>
-  
-<br/>
-  <br/>
+                  </ul> </Fade>
+                  <Fade left> <p>
+                    Lab capacity , Specifications of the lab
+                    
+                  </p> 
+                </Fade>
+              </div>
+            </div>
+              
+            <br/>
+            <br/>
 
-<div class="row">
-  <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-<Flip left> <img src="assets/images/testimonials-4.jpg"></img> </Flip> 
-  </div>
-  <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
-   <Fade left>  <h3> Mobile App Development Lab</h3> </Fade>
-   <Fade left> <p class="font-italic">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua.
-    </p> </Fade>
-   <Fade left> <ul>
-      <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-      <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-      <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-    </ul> </Fade>
-    <Fade left> <p>
-      Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-    </p> </Fade>
-  </div>
-</div>
-<br/>
-  <br/>
+            <div class="row menu">
+              <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <Flip left> <img src="assets/images/model-img1.jpg"></img> </Flip> 
+              </div>
+              <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+              <Fade left>  <h3> Mobile App Development Lab</h3> </Fade>
+              <Fade left> <p class="font-italic">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                  magna aliqua.
+                </p> </Fade>
+              <Fade left> <ul>
+                  <li><i class="ri-check-double-line"></i> <span>Ullamco laboris nisi ut aliquip ex ea commodo consequat.</span></li>
+                  <li><i class="ri-check-double-line"></i> <span>Duis aute irure dolor in reprehenderit in voluptate velit.</span></li>
+                  <li><i class="ri-check-double-line"></i><span> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</span></li>
+                </ul> </Fade>
+                <Fade left> <p>
+                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                </p> </Fade>
+              </div>
+            </div>
+            <br/>
+            <br/>
 
-<div class="row">
-  <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-<Flip left> <img src="assets/images/testimonials-4.jpg"></img> </Flip> 
-  </div>
-  <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
-   <Fade left>  <h3> Web Technologies Lab</h3> </Fade>
-   <Fade left> <p class="font-italic">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua.
-    </p> </Fade>
-   <Fade left> <ul>
-      <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-      <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-      <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-    </ul> </Fade>
-    <Fade left> <p>
-      Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-    </p> </Fade>
-  </div>
-</div>
-<br/>
-  <br/>
+            <div class="row">
+              <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <Flip left> <img src="assets/images/model-img1.jpg"></img> </Flip> 
+              </div>
+              <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+              <Fade left>  <h3> Web Technologies Lab</h3> </Fade>
+              <Fade left> <p class="font-italic">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                  magna aliqua.
+                </p> </Fade>
+              <Fade left> <ul>
+                  <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                  <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                  <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                </ul> </Fade>
+                <Fade left> <p>
+                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                </p> </Fade>
+              </div>
+            </div>
+            <br/>
+            <br/>
 
-<div class="row">
-  <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-<Flip left> <img src="assets/images/testimonials-4.jpg"></img> </Flip> 
-  </div>
-  <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
-   <Fade left>  <h3> DBMS Lab</h3> </Fade>
-   <Fade left> <p class="font-italic">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua.
-    </p> </Fade>
-   <Fade left> <ul>
-      <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-      <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-      <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-    </ul> </Fade>
-    <Fade left> <p>
-      Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-    </p> </Fade>
-  </div>
-</div>
-<br/>
-  <br/>
+            <div class="row">
+              <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <Flip left> <img src="assets/images/model-img1.jpg"></img> </Flip> 
+              </div>
+              <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+              <Fade left>  <h3> DBMS Lab</h3> </Fade>
+              <Fade left> <p class="font-italic">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                  magna aliqua.
+                </p> </Fade>
+              <Fade left> <ul>
+                  <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                  <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                  <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                </ul> </Fade>
+                <Fade left> <p>
+                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                </p> </Fade>
+              </div>
+            </div>
+            <br/>
+            <br/>
 
-<div class="row">
-  <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
-<Flip left> <img src="assets/images/testimonials-4.jpg"></img> </Flip> 
-  </div>
-  <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
-   <Fade left>  <h3> SE Lab </h3> </Fade>
-   <Fade left> <p class="font-italic">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-      magna aliqua.
-    </p> </Fade>
-   <Fade left> <ul>
-      <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
-      <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
-      <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
-    </ul> </Fade>
-    <Fade left> <p>
-      Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
-    </p> </Fade>
-  </div>
-</div>
+            <div class="row">
+              <div class="col-lg-6 order-1 order-lg-2" data-aos="fade-left" data-aos-delay="100">
+            <Flip left> <img src="assets/images/model-img1.jpg"></img> </Flip> 
+              </div>
+              <div class="col-lg-6 pt-4 pt-lg-0 order-2 order-lg-1 content" data-aos="fade-right" data-aos-delay="100">
+              <Fade left>  <h3> SE Lab </h3> </Fade>
+              <Fade left> <p class="font-italic">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+                  magna aliqua.
+                </p> </Fade>
+              <Fade left> <ul>
+                  <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+                  <li><i class="ri-check-double-line"></i> Duis aute irure dolor in reprehenderit in voluptate velit.</li>
+                  <li><i class="ri-check-double-line"></i> Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate trideta storacalaperda mastiro dolore eu fugiat nulla pariatur.</li>
+                </ul> </Fade>
+                <Fade left> <p>
+                  Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident
+                </p> </Fade>
+              </div>
+            </div>
 
-</div>
-
-
-
+            </div>
 
 
-                
             </div>
         )
     }
