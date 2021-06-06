f8e888548f4aaee03701cf80a44e18687d706e89
divyaraman2905 <divyaraman2905@gmail.com>
2021-06-06 20:51:49 +0530

    backend code formatted

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import './App.css';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 import Developers from "./components/Developers";
 import ComplaintBox from "./components/ComplaintBox";
 import Certifications from "./components/Admin/Certifications";
@@ -45,116 +45,106 @@ import AdminWorkshop from "./components/Admin/AdminWorkshops";
 import AlumniForm from "./components/Admin/AlumniForm";
 import Announcements from "./components/Admin/Announcements";
 import Reports from "./components/Admin/Report/Report";
-import Year1 from  "./components/Faculty/Year1";
-import Year2 from  "./components/Faculty/Year2";
-import Year3 from  "./components/Faculty/Year3";
+import Year1 from "./components/Faculty/Year1";
+import Year2 from "./components/Faculty/Year2";
+import Year3 from "./components/Faculty/Year3";
 import Covid from "./components/Covid";
 import FacultyLogin from "./components/FacultyLogin/FacultyLogin";
 import Aboutus from "./components/Aboutus";
 import Logout from "./components/Admin/Logout/Logout";
 import Facultyregistration from "./components/Facultyregistration/Facultyregistration";
 
-
 const DefaultContainer = () => (
   <React.Fragment>
-   
-    <Header/>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/header" component={Header}/>
-        <Route exact path="/infrastructure_seminar" component={Seminar}/>
-        <Route exact path="/faculty_year1" component={Year1}/>
-        <Route exact path="/faculty_year2" component={Year2}/>
-        <Route exact path="/faculty_year3" component={Year3}/>
-        <Route exact path="/infrastructure_labs" component={Lab}/>
-        <Route exact path="/developers" component={Developers}/>
-        <Route exact path="/complaint" component={ComplaintBox}/>
-        <Route exact path="/studentcorner_btech_year1_ai" component={Ai1}/>
-        <Route exact path="/studentcorner_btech_year2_ai" component={Ai2}/>
-        <Route exact path="/studentcorner_btech_year3_ai" component={Ai3}/>
-        <Route exact path="/studentcorner_btech_year4_ai" component={Ai4}/>
-        <Route exact path="/studentcorner_btech_year1_cs" component={Cs1}/>
-        <Route exact path="/studentcorner_btech_year3_cs" component={Cs3}/>
-        <Route exact path="/studentcorner_btech_year2_cs" component={Cs2}/>
-        <Route exact path="/studentcorner_btech_year4_cs" component={Cs4}/>
-        <Route exact path="/studentcorner_btech_year1_cse" component={Cse1}/>
-        <Route exact path="/studentcorner_btech_year2_cse" component={Cse2}/>
-        <Route exact path="/studentcorner_btech_year3_cse" component={Cse3}/>
-        <Route exact path="/studentcorner_btech_year4_cse" component={Cse4}/>
-        <Route exact path="/studentcorner_btech_year1_ds" component={Ds1}/>
-        <Route exact path="/studentcorner_btech_year2_ds" component={Ds2}/>
-        <Route exact path="/studentcorner_btech_year3_ds" component={Ds3}/>
-        <Route exact path="/studentcorner_btech_year4_ds" component={Ds4}/>
-        <Route exact path="/studentcorner_mtech_year1" component={Mfirstyear}/>
-        <Route exact path="/studentcorner_mtech_year2" component={Msecondyear}/>
-        <Route exact path="/footer" component={Footer}/>
-        <Route exact path="/infrastructure_library" component={Library}/>
-        <Route exact path="/workshops_year1" component={workshops_year1}/>
-        <Route exact path="/workshops_year2" component={workshops_year2}/>
-        <Route exact path="/workshops_year3" component={workshops_year3}/>
-        <Route exact path="/research_year3" component={research_year3}/>
-        <Route exact path="/research_year1" component={research_year1}/>
-        <Route exact path="/research_year2" component={research_year2}/>
-        <Route exact path="/alumni" component={Alumni}/>
-        <Route exact path="/events" component={Events}/>
-        <Route exact path="/covid" component={Covid}/>
-        <Route exact path="/about" component={Aboutus}/>
+    <Header />
+    <Route exact path="/" component={Home} />
+    <Route exact path="/header" component={Header} />
+    <Route exact path="/infrastructure_seminar" component={Seminar} />
+    <Route exact path="/faculty_year1" component={Year1} />
+    <Route exact path="/faculty_year2" component={Year2} />
+    <Route exact path="/faculty_year3" component={Year3} />
+    <Route exact path="/infrastructure_labs" component={Lab} />
+    <Route exact path="/developers" component={Developers} />
+    <Route exact path="/complaint" component={ComplaintBox} />
+    <Route exact path="/studentcorner_btech_year1_ai" component={Ai1} />
+    <Route exact path="/studentcorner_btech_year2_ai" component={Ai2} />
+    <Route exact path="/studentcorner_btech_year3_ai" component={Ai3} />
+    <Route exact path="/studentcorner_btech_year4_ai" component={Ai4} />
+    <Route exact path="/studentcorner_btech_year1_cs" component={Cs1} />
+    <Route exact path="/studentcorner_btech_year3_cs" component={Cs3} />
+    <Route exact path="/studentcorner_btech_year2_cs" component={Cs2} />
+    <Route exact path="/studentcorner_btech_year4_cs" component={Cs4} />
+    <Route exact path="/studentcorner_btech_year1_cse" component={Cse1} />
+    <Route exact path="/studentcorner_btech_year2_cse" component={Cse2} />
+    <Route exact path="/studentcorner_btech_year3_cse" component={Cse3} />
+    <Route exact path="/studentcorner_btech_year4_cse" component={Cse4} />
+    <Route exact path="/studentcorner_btech_year1_ds" component={Ds1} />
+    <Route exact path="/studentcorner_btech_year2_ds" component={Ds2} />
+    <Route exact path="/studentcorner_btech_year3_ds" component={Ds3} />
+    <Route exact path="/studentcorner_btech_year4_ds" component={Ds4} />
+    <Route exact path="/studentcorner_mtech_year1" component={Mfirstyear} />
+    <Route exact path="/studentcorner_mtech_year2" component={Msecondyear} />
+    <Route exact path="/footer" component={Footer} />
+    <Route exact path="/infrastructure_library" component={Library} />
+    <Route exact path="/workshops_year1" component={workshops_year1} />
+    <Route exact path="/workshops_year2" component={workshops_year2} />
+    <Route exact path="/workshops_year3" component={workshops_year3} />
+    <Route exact path="/research_year3" component={research_year3} />
+    <Route exact path="/research_year1" component={research_year1} />
+    <Route exact path="/research_year2" component={research_year2} />
+    <Route exact path="/alumni" component={Alumni} />
+    <Route exact path="/events" component={Events} />
+    <Route exact path="/covid" component={Covid} />
+    <Route exact path="/about" component={Aboutus} />
 
-        
     <Footer />
   </React.Fragment>
 );
 
-
 const AdminContainer = () => (
   <React.Fragment>
-
-    
-
-    <Route exact path="/admin/adminheader" component={AdminHeader}/>
+    <Route exact path="/admin/adminheader" component={AdminHeader} />
     <Route path="/admin/login" exact component={Login} />
-    <Route path="/admin/AdminComplaintBox" exact component={AdminComplaintBox} />
+    <Route
+      path="/admin/AdminComplaintBox"
+      exact
+      component={AdminComplaintBox}
+    />
     <Route path="/admin/AdminResearch" exact component={AdminResearch} />
-    <Route path="/admin/AdminStudentCorner" exact component={AdminStudentCorner} />
+    <Route
+      path="/admin/AdminStudentCorner"
+      exact
+      component={AdminStudentCorner}
+    />
     <Route path="/admin/AdminWorkshops" exact component={AdminWorkshop} />
     <Route path="/admin/AlumniForm" exact component={AlumniForm} />
     <Route path="/admin/Announcements" exact component={Announcements} />
     <Route path="/admin/Report" exact component={Reports} />
     <Route path="/admin/logout" exact component={Logout} />
-    
-
   </React.Fragment>
-)
+);
 
 const FacultyContainer = () => (
   <React.Fragment>
-
-    
-
-    <Route exact path="/faculty/login" component={FacultyLogin}/>
-    <Route exact path="/faculty/registration" component={Facultyregistration}/>
+    <Route exact path="/faculty/login" component={FacultyLogin} />
+    <Route exact path="/faculty/registration" component={Facultyregistration} />
     <Route path="/faculty/Certifications" exact component={Certifications} />
-
   </React.Fragment>
-)
+);
 
-const TestContainer = () => (
-  <React.Fragment>
-    
-  </React.Fragment>
-)
-
+const TestContainer = () => <React.Fragment></React.Fragment>;
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Switch>
-            
-          {process.env.NODE_ENV === "development" && <Route path="/(test)" component={TestContainer} />}
+          {process.env.NODE_ENV === "development" && (
+            <Route path="/(test)" component={TestContainer} />
+          )}
           <Route path="/(admin)" component={AdminContainer} />
           <Route path="/(faculty)" component={FacultyContainer} />
           <Route component={DefaultContainer} />
-
         </Switch>
       </div>
     </Router>
--- src/components/Facultyregistration/Facultyregistration.jsx
+++ src/components/Facultyregistration/Facultyregistration.jsx
@@ -1,31 +1,56 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './registration.css';
-
-
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./registration.css";
 
 export default class Facultyregistration extends React.Component {
-    render() {
-        return (
-            <div class="registration">
-                 <div class="card card-container">
-                    
-                    <img id="profile-img" class="profile-img-card" src="//ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-                    <p id="profile-name" class="profile-name-card"></p>
-                    <form class="form-signup">
-                        <span id="reauth-name" class="reauth-name"></span>
-                        <input type="text" id="inputfacultyname" class="form-control" placeholder="Faculty Name" required autofocus/>
-                        <input type="text" id="inputfacultyemail" class="form-control" placeholder="CVR Email id" required autofocus/>
-                        <input type="text" id="inputusername" class="form-control" placeholder="User Name" required autofocus/>
-                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required/>
-                        
-                        <button class="btn btn-lg btn-primary btn-block btn-signup" type="submit">Sign up</button>
-                    </form>
-                    
-                </div>
-            
+  render() {
+    return (
+      <div class="registration">
+        <div class="card card-container">
+          <p id="profile-name" class="profile-name-card"></p>
+          <form class="form-signup">
+            <span id="reauth-name" class="reauth-name"></span>
+            <input
+              type="text"
+              id="inputfacultyname"
+              class="form-control"
+              placeholder="Faculty Name"
+              required
+              autofocus
+            />
+            <input
+              type="text"
+              id="inputfacultyemail"
+              class="form-control"
+              placeholder="CVR Email id"
+              required
+              autofocus
+            />
+            <input
+              type="text"
+              id="inputusername"
+              class="form-control"
+              placeholder="User Name"
+              required
+              autofocus
+            />
+            <input
+              type="password"
+              id="inputPassword"
+              class="form-control"
+              placeholder="Password"
+              required
+            />
 
-            </div>
-        )
-    }
+            <button
+              class="btn btn-lg btn-primary btn-block btn-signup"
+              type="submit"
+            >
+              Sign up
+            </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
 }
